4c6292d5b119945f3d47c4abe117f904f9213bfe
cassandra-mn <arthenciac@gmail.com>
2022-02-08 14:43:36 -0300

    versão com tela inicial e tela aguardando

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Tela inicial 
 function entrar(botao) {
-    botao.parentNode.classList.remove("tela-inicial");
+    const div = botao.parentNode;
+    div.innerHTML = `
+    <div class="tela-aguardando">
+        <img src="images/logo.svg" alt="logo">
+        <div class="loading"><img src="images/load.png" alt="loading"></div>
+        <p>Entrando...</p>
+    </div>`
+    setTimeout(aguardando, 2000);
+}
+
+// Tela aguardando 
+function aguardando() {
+    const telaInicial = document.querySelector(".tela-inicial");
+    telaInicial.classList.remove("tela-inicial");
+    const telaAguardando = document.querySelector(".tela-aguardando");
+    telaAguardando.classList.remove("tela-aguardando");
+    telaAguardando.innerHTML = "";
 }
