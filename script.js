be24f9d62db3a482fb41895e9582cd8e1d44cafe
cassandra-mn <arthenciac@gmail.com>
2022-02-09 02:06:26 -0300

    versão com mensagens exibidas

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Tela inicial 
+
 function entrar(botao) {
     const div = botao.parentNode;
     div.innerHTML = `
@@ -11,6 +12,7 @@ function entrar(botao) {
 }
 
 // Tela aguardando 
+
 function aguardando() {
     const telaInicial = document.querySelector(".tela-inicial");
     telaInicial.classList.remove("tela-inicial");
@@ -18,3 +20,75 @@ function aguardando() {
     telaAguardando.classList.remove("tela-aguardando");
     telaAguardando.innerHTML = "";
 }
+
+// Barra lateral 
+
+function barraLateral() {
+    const fundoMenu = document.querySelector(".fundo-menu");
+    fundoMenu.classList.remove("escondido");
+    const menu = document.querySelector(".menu");
+    menu.classList.remove("escondido");
+}
+
+function esconderMenu() {
+    const fundoMenu = document.querySelector(".fundo-menu");
+    fundoMenu.classList.add("escondido");
+    const menu = document.querySelector(".menu");
+    menu.classList.add("escondido");
+}
+
+// Buscar mensagem do servidor
+
+function buscarDados(resposta) {
+    let dados = resposta.data;
+    imprimirMensagens(dados);
+}
+
+// Imprimir mensagens na tela
+
+function imprimirMensagens(dados) {
+    const mensagem = document.querySelector(".container");
+    for (let i = 0; i < dados.length; i++) {   
+        if (dados[i].type === "status") {
+            mensagemStatus(mensagem, dados[i]);
+        }   
+        else if (dados[i].type === "message") {
+            mensagemPublica(mensagem, dados[i]);
+        }
+        else {
+            mensagemPrivada(mensagem, dados[i]);
+        }
+    }
+}
+
+function mensagemStatus(mensagem, dados) {
+    mensagem.innerHTML += `
+    <div class="mensagem fundo-cinza">
+        <p class="horario">${dados.time}</p>
+        <p class="nome">${dados.from}</p>
+        <p class="acao">${dados.text}</p>
+    </div>`
+}
+
+function mensagemPublica(mensagem, dados) {
+    mensagem.innerHTML += `
+    <div class="mensagem fundo-branco">
+        <p class="horario">${dados.time}</p>
+        <p class="nome">${dados.from}</p>
+        <p class="para"> para <strong>${dados.to}</strong></p>
+        <p class="texto">: ${dados.text}</p>
+    </div>`
+}
+
+function mensagemPrivada(mensagem, dados) {
+    mensagem.innerHTML += `
+    <div class="mensagem fundo-rosa">
+        <p class="horario">${dados.time}</p>
+        <p class="nome">${dados.from}</p>
+        <p class="para"> reservadamente para <strong>${dados.to}</strong></p>
+        <p class="texto">: ${dados.text}</p>
+    </div>`
+}
+
+const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+promise.then(buscarDados);
